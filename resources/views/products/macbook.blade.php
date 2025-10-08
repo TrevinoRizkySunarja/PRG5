0d8c7755dba42dd652e914de251f1fc4ad8ba99f
TrevinoRizkySunarja <trevprive123@gmail.com>
2025-10-08 12:57:20 +0200

    Initial Commit v2

---
+++ resources/views/products/macbook.blade.php
@@ -0,0 +1,2 @@
+<h1>Dit is {{$macbook}}</h1>
+<p>Dit is de macbook pagina</p>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/about-us', function() {
+    $company = 'Hogeschool Rotterdam';
+    return view('about-us', [
+        'company' => $company
+    ]);
+});
+
+
+Route::get('products/{macbook}', function(string $macbook) {
+    return view ('products.macbook',
+        ['macbook' => $macbook]);
+//    return view ('products.macbook',compact('macbook'));
+});
+
+
+
+
+Route::get('/contact-us', function() {
+   return view ('This page is contact us');
+});
+
+
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
